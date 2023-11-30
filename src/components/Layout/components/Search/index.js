2ab7dc392fcb7b03fdaa2f6ee0ce5a4bee0a4dcf
Nguyen Tien Dan <nguyen.tien.dan@sun-asterisk.com>
2023-11-30 15:19:26 +0700

    Logic HEADER 3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -6,6 +6,7 @@ import { faCircleQuestion, faCircleXmark, faEarthAsia, faEllipsisVertical, faEur
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
 import { useEffect, useRef, useState } from 'react';
+import { useDebounce } from '~/hooks';
 
 const cx = classNames.bind(styles)
 
@@ -15,18 +16,20 @@ function Search() {
     const [showSearchResult, setShowSearchResult] = useState(true)
     const [loading, setLoading] = useState(false)
 
+    // 1: ''
+    const debounced = useDebounce(searchValue, 500)
 
     const handleHideSearchResult = () => {
         setShowSearchResult(false)
     }
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([])
             return
         }
         setLoading(true)
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then(res => res.json())
             .then(res => {
                 setSearchResult(res.data);
@@ -35,7 +38,7 @@ function Search() {
             .catch(() => {
                 setLoading(false)
             })
-    }, [searchValue])
+    }, [debounced])
 
     const inputRef = useRef()
     return (
